71f48c39a58fc4ba1b1fde8ab50c3d593a2b599e
gustavoksbr <gustavosalesi@hotmail.com>
2024-06-11 15:49:21 -0300

    mensagem

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MensagemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//mensagem
+Route::get("/mensagem/{mensagem}", [MensagemController::class, 'mostrarMensagem']);
